74d9ba32a416afda93ce631ea0952059c94be77d
Vishnu Yelde <vishnuyelde@gmail.com>
2025-12-12 23:45:46 +0530

    Request Mapping used for Common URL

--- SB2_REST_APIs/src/main/java/boot/controller/StudentCtrl.java
+++ SB2_REST_APIs/src/main/java/boot/controller/StudentCtrl.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/student") // used to give common URL
 public class StudentCtrl {
 
 	@PostMapping("/save")
