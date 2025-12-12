671ed993056f89debcf6d7e2b217e2a94768fa4f
Vishnu Yelde <vishnuyelde@gmail.com>
2025-12-13 00:10:09 +0530

    other files

---
+++ SB3_Product_Service_APIs/src/main/java/boot/Sb3ProductServiceApIsApplication.java
@@ -0,0 +1,13 @@
+package boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3ProductServiceApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb3ProductServiceApIsApplication.class, args);
+	}
+
+}
