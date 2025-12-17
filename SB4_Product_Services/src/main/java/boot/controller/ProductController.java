ea19736133a5f0b0a332cf6c41aa2fbbe73e6b97
Vishnu Yelde <vishnuyelde@gmail.com>
2025-12-18 02:46:31 +0530

    Create API Pagination & Sorting

--- SB4_Product_Services/src/main/java/boot/controller/ProductController.java
+++ SB4_Product_Services/src/main/java/boot/controller/ProductController.java
@@ -9,6 +9,7 @@ import boot.services.ProductService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,37 +24,50 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/prod")
 public class ProductController {
 
-	@Autowired	
+	@Autowired
 	private ProductService productService;
-	
+
 	// Inserting Product Records in Table
 	@PostMapping("/save")
 	public Product InsertProduct(@RequestBody Product product) {
 		return productService.save(product); // Insert the data Object and returns it.
 	}
-	
+
 	// Fetch all Data
 	@GetMapping("/getall")
 	public List<Product> getAllProducts() {
 		return productService.findAllRecords();
 	}
-	
-	//Fetch Data by ID
+
+	// Fetch Data by ID
 	@GetMapping("/getbyid")
 	public Product getById(@RequestParam Integer pid) {
 		return productService.getById(pid);
 	}
-	
+
 	// Delete Record By Id
 	@DeleteMapping("/deletebyid/{pid}")
 	public String delById(@PathVariable Integer pid) {
 		return productService.deleteById(pid);
 	}
-	
+
 	// Update Product Record
 	@PutMapping("/update/{id}")
 	public Product updateProduct(@PathVariable Integer id, @RequestBody Product product) {
 		return productService.updatProduct(id, product);
 	}
-	
+
+	// Pagination API (Used to Distribute records in Single page)
+	@GetMapping("/fetchpage")
+	public Page<Product> fetchByPage(@RequestParam Integer pageNo) {
+		Page<Product> products = productService.fetchByPage(pageNo);
+		return products;
+	}
+
+	// Sorting API (Used to sort product based on parameter) order --> asc, desc
+	@GetMapping("/sort")
+	public List<Product> sortProduct(@RequestParam(required = false) String param, @RequestParam(required = false) String order) {
+		return productService.sortingProd(param, order);
+	}
+
 }
--- SB4_Product_Services/src/main/java/boot/services/ProductService.java
+++ SB4_Product_Services/src/main/java/boot/services/ProductService.java
@@ -3,6 +3,10 @@ package boot.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import boot.dao.ProductDAO;
@@ -16,23 +20,23 @@ public class ProductService {
 
 	@Autowired
 	private ProductDAO productDAO;
-	
-	@Autowired 
+
+	@Autowired
 	private ProductRepo productRepo;
-	
+
 	// to insert the record/Data
 	public Product save(Product product) {
 		return productDAO.save(product);
 	}
-	
-	// to fetch data by id 
+
+	// to fetch data by id
 	public Product getById(Integer pid) {
 		return productDAO.getById(pid);
 	}
-	
+
 	// to Update the Data/Record
 	public Product updatProduct(Integer pid, Product product) {
-		
+
 		Product dbPrevProduct = productDAO.getById(pid);
 		if (dbPrevProduct.getName() != null) {
 			dbPrevProduct.setName(product.getName());
@@ -46,22 +50,39 @@ public class ProductService {
 		if (dbPrevProduct.getColor() != null) {
 			dbPrevProduct.setColor(product.getColor());
 		}
-		
+
 		Product updatedProd = productDAO.save(dbPrevProduct);
-		
+
 		return updatedProd;
 	}
-	
-	// to Delete record by Id 
+
+	// to Delete record by Id
 	public String deleteById(Integer pid) {
 		Product dbPrevProduct = productDAO.getById(pid);
 		productDAO.delete(dbPrevProduct);
-		
+
 		return "Product Delete Successfully";
 	}
-	
+
 	// to Fetch all records
 	public List<Product> findAllRecords() {
 		return productRepo.findAll();
 	}
+
+	// Pagination list of products in single page
+	public Page<Product> fetchByPage(Integer pageNo) {
+		Pageable pageable = PageRequest.of(pageNo - 1, 15);
+		Page<Product> pageProd = productRepo.findAll(pageable);
+		return pageProd;
+	}
+
+	// Sorting the products in order --> asc & desc
+	// parameter means column name
+	public List<Product> sortingProd(String param, String order) {
+		if (order != null && order.equalsIgnoreCase("desc")) {
+			return productRepo.findAll(Sort.by(param).descending());// By default its ascending order
+		}
+		return productRepo.findAll(Sort.by(param).ascending());
+	}
+
 }
