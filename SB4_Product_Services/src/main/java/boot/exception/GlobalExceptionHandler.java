041a9e22b9a25b252c2b7a0645e1ee49e81e18c8
Vishnu Yelde <vishnuyelde@gmail.com>
2025-12-22 23:57:17 +0530

    all Exceptions Handled Globally

---
+++ SB4_Product_Services/src/main/java/boot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package boot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(ArithmeticException.class)
+	public ResponseEntity<String> arithmeticExcepHandler(ArithmeticException arithmeticException) {
+		System.out.println("AE exception Handled Globally");
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(arithmeticException.getMessage());
+	}
+	
+	@ExceptionHandler(NullPointerException.class)
+	public ResponseEntity<String> nullPointExcepHandler(NullPointerException nullPointerException) {
+		System.out.println("NPE Handled Globally");
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(nullPointerException.getMessage());
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> excepHandler(Exception exception) {
+		System.out.println("All Exception Handled Globally");
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+	}
+}
