b69dba13889f71a7f736605d77f72e73c17bbde1
Vishnu Yelde <vishnuyelde@gmail.com>
2025-12-25 22:01:18 +0530

    H2 Database(Temporary DB) implemented

---
+++ SB5_H2DB_Product_Services/src/test/java/boot/Sb5H2DbProductServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb5H2DbProductServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
