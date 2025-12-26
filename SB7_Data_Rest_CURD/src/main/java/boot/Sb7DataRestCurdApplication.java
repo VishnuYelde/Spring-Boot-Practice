5357146a7faca4cff41baee51b748116116aabc5
Vishnu Yelde <vishnuyelde@gmail.com>
2025-12-26 20:47:38 +0530

    Data Rest Implementing

---
+++ SB7_Data_Rest_CURD/src/main/java/boot/Sb7DataRestCurdApplication.java
@@ -0,0 +1,13 @@
+package boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb7DataRestCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb7DataRestCurdApplication.class, args);
+	}
+
+}
