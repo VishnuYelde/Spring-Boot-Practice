27b096b8af8d9c28dad617193ba0831a57285a8b
Vishnu Yelde <vishnuyelde@gmail.com>
2025-12-30 20:36:50 +0530

    In Memory Authentication and Authorization

---
+++ SB9_Security_In_Memory/src/main/java/boot/Sb9SecurityInMemoryApplication.java
@@ -0,0 +1,13 @@
+package boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb9SecurityInMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb9SecurityInMemoryApplication.class, args);
+	}
+
+}
