1ae3c89e13b94ad4045e5ac1e1077b24dc8e983a
Vishnu Yelde <vishnuyelde@gmail.com>
2025-12-31 22:30:33 +0530

    Data JPA Authentication and Authorization implemented

---
+++ SB10_Security_Data_JPA/src/main/java/boot/Sb10SecurityDataJpaApplication.java
@@ -0,0 +1,13 @@
+package boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb10SecurityDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb10SecurityDataJpaApplication.class, args);
+	}
+
+}
---
+++ SB10_Security_Data_JPA/src/test/java/boot/Sb10SecurityDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb10SecurityDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
